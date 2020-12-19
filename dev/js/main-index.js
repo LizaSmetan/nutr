31be594a3745be5164d5723facd165da2133b70d
LizaSmetan <krotovaliza@gmail.com>
2020-12-19 02:04:02 +0200

    main1 videos

--- dev/js/main-index.js
+++ dev/js/main-index.js
@@ -1,10 +1,12 @@
 import '../scss/styles-main.scss';
 
-import $ from './jquery-3.5.1.min';
+import $ from 'jquery';
 import './bootstrap.bundle.min';
-import './select2.full.min';
-import './slick.min';
-import './jquery.scrollbar.min';
+import select2 from './select2.full.min';
+import slick from 'slick-carousel';
+import scrollbar from './jquery.scrollbar.min';
+import WOW from "wowjs";
+
 
 $().ready(function () {
     function setHeightPage() {
@@ -18,5 +20,33 @@ $().ready(function () {
 
     $(document).on('click', '.toggle_menu_js', function () {
         $('html').toggleClass('opened-menu');
-    })
+    });
+    new WOW.WOW().init();
+
+    $('.top_slider_js').slick({
+        slidesToShow: 6,
+        dots: false,
+        arrows: true,
+        infinite: false
+    });
+
+    $('.select2').select2({
+        width: '100%',
+        minimumResultsForSearch: 20
+    }).on('select2:open', function () {
+        $('.select2-container--default .select2-results > .select2-results__options').addClass('scrollbar-inner').scrollbar();
+    });
+
+    $('.modal').on('hide.bs.modal', function () {
+        if($(this).find('video').length > 0 && $(this).find('video')[0]){
+            var video = $(this).find('video')[0];
+
+            if (video && video.paused != true) {
+                video.pause();
+            }
+        }
+
+    });
+    
+
 });
